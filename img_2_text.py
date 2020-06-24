3417a46085b442bdf504da49b43d63ba88d134c7
Senthilkumar <sksenthilkumar93@gmail.com>
2020-06-24 22:32:15 +0200

    move combined_pdf as function in the main script

--- img_2_text.py
+++ img_2_text.py
@@ -41,7 +41,7 @@ def create_sub_folder(folder: pathlib.Path, sub_folder_name: str):
     return subfolder_path
 
 
-def convert_images(image_files: list, output_folder: pathlib.Path, output_file_format: str):
+def convert_images_and_save(image_files: list, output_folder: pathlib.Path, output_file_format: str):
     image_files.sort()
     assert output_file_format in ['pdf', 'txt']
     output_folder_path = create_sub_folder(output_folder, f"{output_file_format}s")
@@ -61,12 +61,39 @@ def convert_images(image_files: list, output_folder: pathlib.Path, output_file_f
         save_txt_file_fullpath = f"{str(output_folder_path)}/{image_name}.{output_file_format}"
         save_function(converted_image, save_txt_file_fullpath)
 
+    return str(output_folder_path)
+
+
+def combine_pdf(folder: str, output_file_name='combined'):
+    from PyPDF2 import PdfFileMerger
+    import glob
+
+    folder_path = pathlib.Path(folder)
+    if not folder_path.exists():
+        raise FileNotFoundError
+
+    pdfs = glob.glob(f"{folder}/*.pdf")
+    if not pdfs:
+        raise ValueError(f"Empty Folder: No pdfs found in the path {folder}")
+    pdfs.sort()
+
+    merger = PdfFileMerger()
+    for pdf in pdfs:
+        merger.append(pdf)
+
+    output_file_name = f"{output_file_name}.pdf"
+    merger.write(output_file_name)
+    merger.close()
+
+    return output_file_name
+
 
 def main():
     image_filename_search = f"{args.images_folder}/*.{args.images_extension}"
     image_files = glob.glob(image_filename_search)
-    convert_images(image_files, pathlib.Path(args.output_folder), output_file_format='pdf')
-    convert_images(image_files, pathlib.Path(args.output_folder), output_file_format='txt')
+    pdfs_saved_path = convert_images_and_save(image_files, pathlib.Path(args.output_folder), output_file_format='pdf')
+    final_pdf = combine_pdf(pdfs_saved_path)
+    txts_saved_path = convert_images_and_save(image_files, pathlib.Path(args.output_folder), output_file_format='txt')
 
 
 if __name__ == '__main__':
